b1dc846e2a01a258eb8fd6117a3cbc5984f51327
Ziegler Torben <tz023@hdm-stuttgart.de>
2024-04-19 17:17:29 +0200

    feat: page translations

---
+++ frontend/src/i18n/ui.ts
@@ -0,0 +1,33 @@
+export const languages = {
+  de: 'Deutsch',
+  en: 'English',
+}
+
+export const defaultLang = 'de'
+
+export const ui = {
+  en: {
+    title: 'Heating Control',
+    description: 'Tool for controling the heating in the PUX Lab',
+    'nav.home': 'Home',
+    'nav.settings': 'Settings',
+    'nav.help': 'Help',
+    'nav.help.userManual': 'User manual',
+    'nav.help.installationGuide': 'Installation guide',
+    index: 'Index',
+    workInProgress: 'Work in progress',
+    goToGitlab: 'Go to Gitlab repository',
+  },
+  de: {
+    title: 'Heizungssteuerung',
+    description: 'Heizungssteuerung Tool für die Steuerung von Heizungen im PUX Lab',
+    'nav.home': 'Start',
+    'nav.settings': 'Einstellungen',
+    'nav.help': 'Hilfe',
+    'nav.help.userManual': 'Bedienungsanleitung',
+    'nav.help.installationGuide': 'Installationsanleitung',
+    index: 'Index',
+    workInProgress: 'In Arbeit',
+    goToGitlab: 'Zum Gitlab Repository',
+  },
+} as const
