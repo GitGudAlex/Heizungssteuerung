1dbee415a529d39766242af103fb59f3bf50ea90
ak271 <ak271@hdm-stuttgart.de>
2024-06-14 15:42:01 +0200

    Devicelist and floorplan added mapping

--- frontend/src/components/devices/DeviceList.svelte
+++ frontend/src/components/devices/DeviceList.svelte
@@ -52,6 +52,7 @@
             <div class="flex items-center justify-between">
               <h3 class="text-lg leading-6 font-medium text-gray-900">{device.name}</h3>
               <p class="mt-1 max-w-2xl text-sm text-gray-500">{translations['roomMap']} {device.roomMap}</p>
+              <p class="mt-1 max-w-2xl text-sm text-gray-500">{device.heaterMap}</p>
             </div>
             <div class="mt-4 flex items-center justify-between">
               <p class="text-sm font-medium text-gray-500">Status: 
