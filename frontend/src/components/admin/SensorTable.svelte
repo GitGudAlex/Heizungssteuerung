0bda70f10d6ccd7e237ccb285479a887bc7138cb
Torben Ziegler <tz023@hdm-stuttgart.de>
2024-06-14 15:55:02 +0200

    fix: Verification endpoint

--- frontend/src/components/admin/SensorTable.svelte
+++ frontend/src/components/admin/SensorTable.svelte
@@ -110,7 +110,7 @@
 
   const verifyDeviceExistance = async (identifier: string) => {
     try {
-      const response = await fetch(`http://localhost:3000/heating/${identifier}`, {
+      const response = await fetch(`http://localhost:3000/heating-control/${identifier}`, {
         method: 'GET',
       })
       if (!response.ok) {
