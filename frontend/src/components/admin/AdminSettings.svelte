6e91757acbf59afde8f02757560752091615d7cb
Ziegler Torben <tz023@hdm-stuttgart.de>
2024-07-02 15:20:33 +0200

    Fix/improve calendar sync ux

--- frontend/src/components/admin/AdminSettings.svelte
+++ frontend/src/components/admin/AdminSettings.svelte
@@ -3,7 +3,7 @@
   import { onMount } from 'svelte'
   import { writable } from 'svelte/store'
   import { addFontSize } from '~/components/settings/getTextSize.js'
-  import { lineHeight } from '~/components/settings/TextSpaceSetting.svelte';
+  import { lineHeight } from '~/components/settings/TextSpaceSetting.svelte'
   // Props for the component
   export let translations: { [key: string]: string }
   export let lang: string
@@ -33,9 +33,9 @@
         initialSettings = await response.json()
         invitationCode = initialSettings.invitationCode
         buildingOfInterest = initialSettings.buildingOfInterest
-        defaultTemp = initialSettings.defaultTemp
-        preheatingMinutesPerDegree = initialSettings.preheatingMinutesPerDegree
-        isSyncActive = initialSettings.isSyncActive
+        defaultTemp = initialSettings.defaultTemp ?? defaultTemp
+        preheatingMinutesPerDegree = initialSettings.preheatingMinutesPerDegree ?? preheatingMinutesPerDegree
+        isSyncActive = initialSettings.isSyncActive ?? false
         console.log('Settings fetched successfully:', initialSettings)
       } else {
         console.error('Failed to fetch settings:', response.statusText)
@@ -58,7 +58,7 @@
           buildingOfInterest,
           defaultTemp,
           preheatingMinutesPerDegree,
-          isSyncActive
+          isSyncActive,
         }),
       })
       if (response.ok) {
@@ -97,23 +97,115 @@
       document.documentElement.style.setProperty('--add-font-size', cssVar)
     }
   }
-  
+
   $: {
     if (typeof window !== 'undefined' && $lineHeight !== undefined) {
-      const cssVar = `${$lineHeight}`;
-      document.documentElement.style.setProperty('--line-height', cssVar);
+      const cssVar = `${$lineHeight}`
+      document.documentElement.style.setProperty('--line-height', cssVar)
     }
   }
 
   const handleButtonClick = (makeSyncActive: boolean) => {
     isSyncActive = makeSyncActive
-    settingsChanged.set(true);
+    settingsChanged.set(true)
   }
- 
 </script>
 
 <div class="admin-settings-container mx-auto space-y-4">
+<div class="admin-settings-container mx-auto space-y-4">
   <form on:submit|preventDefault={handleSaveSettings}>
+    <!-- Invitation Code Card -->
+    <div
+      class="bg-white dark:bg-gray-800 text-black dark:text-white rounded-lg shadow-lg p-6 mb-4 hover:shadow-xl transition-shadow duration-300"
+    >
+      <h2 class="text-lg font-bold mb-2">{translations['invitationCode']}</h2>
+      <p class="text-sm mb-4">{translations['invitationCodeDescription']}</p>
+      <div class="input input-bordered flex items-center gap-2 mb-4">
+        <input
+          type="text"
+          class="rounded-full px-4 py-2 border border-gray-300 focus:border-blue-500 outline-none bg-transparent w-60 button-font-size"
+          id="invitationCode"
+          placeholder={translations['invitationCode']}
+          bind:value={invitationCode}
+          on:input={handleInputChange}
+        />
+        <button
+          type="button"
+          class="bg-purple-500 dark:bg-purple-700 hover:bg-purple-700 dark:hover:bg-purple-900 text-white font-bold py-3 px-6 rounded-full transition duration-300"
+          on:click={() => copyToClipboard(`http://localhost:4321/${lang}/register?invitationCode=${invitationCode}`)}
+        >
+          {translations['copyLink']}
+        </button>
+      </div>
+    </div>
+
+    <!-- Building of Interest Card -->
+    <div
+      class="bg-white dark:bg-gray-800 text-black dark:text-white rounded-lg shadow-lg p-6 mb-4 hover:shadow-xl transition-shadow duration-300"
+    >
+      <h2 class="text-lg font-bold mb-2">{translations['buildingOfInterest']}</h2>
+      <p class="text-sm mb-4">{translations['buildingOfInterestDescription']}</p>
+      <div class="input input-bordered flex items-center gap-2 mb-4">
+        <input
+          type="text"
+          class="rounded-full px-4 py-2 border border-gray-300 focus:border-blue-500 outline-none bg-transparent w-60 button-font-size"
+          id="buildingOfInterest"
+          placeholder={translations['buildingOfInterest']}
+          bind:value={buildingOfInterest}
+          on:input={handleInputChange}
+        />
+      </div>
+    </div>
+
+    <!-- Default Temperature Card -->
+    <div
+      class="bg-white dark:bg-gray-800 text-black dark:text-white rounded-lg shadow-lg p-6 mb-4 hover:shadow-xl transition-shadow duration-300 card-container-transition {isSyncActive
+        ? ''
+        : 'disabled-card'}"
+    >
+      <h2 class="text-lg font-bold mb-2">{translations['defaultTemp']}</h2>
+      <p class="text-sm mb-4">{translations['defaultTempDescription']}</p>
+      <div class="input input-bordered flex items-center gap-2 mb-4">
+        <input
+          type="text"
+          class="rounded-full px-4 py-2 border border-gray-300 focus:border-blue-500 outline-none bg-transparent w-60 button-font-size"
+          id="defaultTemp"
+          placeholder={translations['defaultTemp']}
+          bind:value={defaultTemp}
+          on:input={handleInputChange}
+          disabled={!isSyncActive}
+        />
+      </div>
+    </div>
+
+    <!-- Preheating Minutes Per Degree  -->
+    <div
+      class="bg-white dark:bg-gray-800 text-black dark:text-white rounded-lg shadow-lg p-6 mb-4 hover:shadow-xl transition-shadow duration-300 card-container-transition {isSyncActive
+        ? ''
+        : 'disabled-card'}"
+    >
+      <h2 class="text-lg font-bold mb-2">{translations['preheatingMinutesPerDegree']}</h2>
+      <p class="text-sm mb-4">{translations['preheatingMinutesPerDegreeDescription']}</p>
+      <div class="input input-bordered flex items-center gap-2 mb-4">
+        <input
+          type="text"
+          class="rounded-full px-4 py-2 border border-gray-300 focus:border-blue-500 outline-none bg-transparent w-80 button-font-size"
+          id="preheatingMinutesPerDegree"
+          placeholder={translations['preheatingMinutesPerDegree']}
+          bind:value={preheatingMinutesPerDegree}
+          on:input={handleInputChange}
+          disabled={!isSyncActive}
+        />
+      </div>
+    </div>
+
+    <!-- Sync Active Card -->
+    <div
+      class=" {!isSyncActive
+        ? 'bg-red-500'
+        : 'bg-white dark:bg-gray-800'} card-container-transition text-black dark:text-white rounded-lg shadow-lg p-6 mb-4 hover:shadow-xl transition-shadow duration-300"
+    >
+      <h2 class="text-lg font-bold mb-2">{translations['isSyncActive']}</h2>
     <!-- Invitation Code Card -->
     <div class="bg-white dark:bg-gray-800 text-black dark:text-white rounded-lg shadow-lg p-6 mb-4 hover:shadow-xl transition-shadow duration-300">
       <h2 class="text-lg font-bold mb-2">{translations['invitationCode']}</h2>
@@ -191,19 +283,19 @@
       <p class="setting-description text-space">{translations['isSyncActiveDescription']}</p>
       <div class="my-8">
         <ButtonGroup>
-            <Button outline checked={true === isSyncActive} color="dark" on:click={() => handleButtonClick(true)}>
-              enable
-            </Button>
-            <Button outline checked={false === isSyncActive} color="dark" on:click={() => handleButtonClick(false)}>
-              disable
-            </Button>
+          <Button outline checked={true === isSyncActive} color="dark" on:click={() => handleButtonClick(true)}>
+            {translations['enable']}
+          </Button>
+          <Button outline checked={false === isSyncActive} color="dark" on:click={() => handleButtonClick(false)}>
+            {translations['disable']}
+          </Button>
         </ButtonGroup>
       </div>
     </div>
 
-
     <!-- Save Button -->
     <button
+      class="rounded-full px-4 py-2 bg-red-700 text-white hover:bg-red-500 transition duration-300 button-font-size"
       class="rounded-full px-4 py-2 bg-red-700 text-white hover:bg-red-500 transition duration-300 button-font-size"
       type="submit"
     >
@@ -232,5 +324,14 @@
   .text-space {
     line-height: var(--line-height);
     transition: line-height 0.5s ease;
+  }
+
+  .disabled-card {
+    opacity: 0.35;
+    pointer-events: none;
+  }
+
+  .card-container-transition {
+    transition: background-color 0.3s ease;
   }
 </style>
