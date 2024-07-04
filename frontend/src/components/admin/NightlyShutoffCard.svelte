62ea3d9c6dfb1d330dbcfb86d0261c09ac787e1e
Alexander Kraus <ak364@hdm-stuttgart.de>
2024-07-04 10:09:17 +0200

    NightlyShutOffCard added text-space and textsize

--- frontend/src/components/admin/NightlyShutoffCard.svelte
+++ frontend/src/components/admin/NightlyShutoffCard.svelte
@@ -24,14 +24,14 @@
 </script>
 
 <div class="bg-white dark:bg-gray-800 text-black dark:text-white rounded-lg shadow-lg p-6 mb-4 hover:shadow-xl transition-shadow duration-300">
-  <h2 class="text-lg font-bold mb-2">{translations['nightlyShutoff']}</h2>
-  <p class="text-sm mb-4">{translations['nightlyShutoffDescription']}</p>
+  <h2 class="text-lg font-bold mb-2 setting-title">{translations['nightlyShutoff']}</h2>
+  <p class="text-sm mb-4 setting-description text-space">{translations['nightlyShutoffDescription']}</p>
   <div class="input input-bordered flex flex-col gap-4">
-    <label class="flex items-center gap-2">
+    <label class="flex items-center gap-2 setting-description text-space">
     {translations['nightlyShutoffOffTime']}
       <input
         type="number"
-        class="rounded-full px-4 py-2 border border-gray-300 focus:border-blue-500 outline-none bg-transparent"
+        class="rounded-full px-4 py-2 border border-gray-300 focus:border-blue-500 outline-none bg-transparent "
         min="0"
         max="23"
         step="1"
@@ -40,7 +40,7 @@
         on:input={(event) => handleSettingChange('off', parseInt(event.target.value))}
       />
     </label>
-    <label class="flex items-center gap-2">
+    <label class="flex items-center gap-2 setting-description text-space">
     {translations['nightlyShutoffOnTime']}
       <input
         type="number"
@@ -55,3 +55,35 @@
     </label>
   </div>
 </div>
+
+<style>
+  .button-font-size {
+    font-size: calc(16px + var(--add-font-size));
+  }
+  .setting-title {
+    flex: 1;
+    margin: 0;
+    font-size: calc(24px + var(--add-font-size));
+    font-weight: bold;
+  }
+
+  .setting-description {
+    flex: 2;
+    margin: 0 1.5em 0.5em 0; /* top right bottom left */
+    font-size: calc(16px + var(--add-font-size));
+  }
+
+  .text-space {
+    line-height: var(--line-height);
+    transition: line-height 0.5s ease;
+  }
+
+  .disabled-card {
+    opacity: 0.35;
+    pointer-events: none;
+  }
+
+  .card-container-transition {
+    transition: background-color 0.3s ease;
+  }
+</style>
