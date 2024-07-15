d8ae8e8b6e2d9ae43a27597032693c625a0f9a42
Florian Demel <florian.demel@meondi.com>
2024-07-15 14:29:38 +0200

    Add openvpn installation to connect_vpn script

--- connect_vpn.sh
+++ connect_vpn.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
+# This script is used to connect to the HDM VPN using OpenVPN.
+# It is meant to be run on a Raspberry Pi or any other Linux-based system. 
+
 # Name der Konfigurationsdatei ohne die Erweiterung (.ovpn)
 CONFIG_FILE="./vpn/hdm_mi_stud_new_ca.ovpn"
 
-# Überprüfen, ob OpenVPN installiert ist
-if ! command -v openvpn &> /dev/null
-then
-    echo "OpenVPN ist nicht installiert. Bitte installiere OpenVPN."
-    exit 1
-fi
+# Funktion zur Überprüfung und Installation von OpenVPN
+install_openvpn() {
+    if ! command -v openvpn &> /dev/null
+    then
+        echo "OpenVPN ist nicht installiert. Installation wird durchgeführt..."
+        sudo apt update
+        sudo apt install -y openvpn
+        if [ $? -ne 0 ]; then
+            echo "Fehler bei der Installation von OpenVPN."
+            exit 1
+        fi
+        echo "OpenVPN erfolgreich installiert."
+    else
+        echo "OpenVPN ist bereits installiert."
+    fi
+}
+
+# Überprüfen und Installieren von OpenVPN
+install_openvpn
 
 # Starten von OpenVPN mit der angegebenen Konfigurationsdatei
 sudo openvpn --config ${CONFIG_FILE}
